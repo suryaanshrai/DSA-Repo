6246ebfb35ccf0f1f3a816ae0d8d44f1a0e22cf5
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-06-30 21:55:19 +0530

    Nearest 1 in a grid

---
+++ graphs/nearest1.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+   public:
+    //Function to find distance of nearest 1 in the grid for each cell.
+	vector<vector<int>>nearest(vector<vector<int>>grid)
+	{
+	    // Code here
+	    vector<vector<int>> ans(grid.size(), vector<int> (grid[0].size(), 0));
+	    vector<vector<int>> vis(grid.size(), vector<int> (grid[0].size(), 0));
+	    queue<pair<pair<int,int>,int>> q;
+	    for (int i = 0; i < grid.size(); i++) {
+	        for (int j = 0; j < grid[0].size(); j++) {
+	            if (grid[i][j] == 1) {
+	                q.push({{i,j}, 0});
+	                vis[i][j] = 1;
+	            }
+	        }
+	    }
+	    while (!q.empty()) {
+	        pair<pair<int,int>,int> temp=q.front();
+	        q.pop();
+	        int x = temp.first.first;
+	        int y = temp.first.second;
+	        int dx[] = {0,1,0,-1};
+	        int dy[] = {1,0,-1,0};
+	        for (int k = 0; k < 4; k++) {
+	            if (x+dx[k] >= 0 && x+dx[k] < grid.size() &&
+	               y+dy[k] >= 0 && y+dy[k] < grid[0].size() &&
+	               vis[x+dx[k]][y+dy[k]] == 0) {
+	                   vis[x+dx[k]][y+dy[k]] = 1;
+	                   ans[x+dx[k]][y+dy[k]] = temp.second+1;
+	                   q.push({{x+dx[k], y+dy[k]}, temp.second+1});
+	               }
+	        }
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.nearest(grid);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
