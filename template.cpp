93ca36915713e5be48853a97d8d9837cacca8b50
suryaanshrai <102371942+suryaanshrai@users.noreply.github.com>
2023-10-05 16:09:44 +0530

    Added template code for C++ programs

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define fr(i,a,b) for (int i = a, a < b; i++)
+#define loop(n) for (int i = 0; i < n; i++)
+#define mod 1000000007
+#define inf (1ll<<60)
+#define all(x) (x).begin, (x).end()
+
+using namespace std;
+
+void precal() {
+    // TODO
+}
+
+void solve() {
+    // TODO
+}
+
+int main() {
+    fast_io;
+    cout << fixed;
+    cout << setprecision(10);
+    precal();
+    int t = 1;
+    // cin >> t;
+    loop(t) {
+        solve();
+    }
+}
