c3b25b320072433e62214dc0401c2c39158a4a58
Suryaansh Rai <102371942+suryaanshrai@users.noreply.github.com>
2024-02-17 15:13:32 +0530

    Update grundy.cpp

--- standardProblems/grundy.cpp
+++ standardProblems/grundy.cpp
@@ -14,13 +14,14 @@ int calculateGrundy (int n) {
     if (n == 0) 
         return 0;
     unordered_set<int> Set;
+    if (grundy.find(n) != gurndy.end()) return grundy[n];
 	if (grundy.find(n/2) == grundy.end()) grundy[n/2] = calculateGrundy(n/2);
 	if (grundy.find(n/3) == grundy.end()) grundy[n/3] = calculateGrundy(n/3);
 	if (grundy.find(n/6) == grundy.end()) grundy[n/6] = calculateGrundy(n/6);
     Set.insert(grundy[n/2]); 
     Set.insert(grundy[n/3]); 
     Set.insert(grundy[n/6]); 
-    grundy[n] = calculateMex(Set)
+    grundy[n] = calculateMex(Set);
     return grundy[n]; 
 } 
 
